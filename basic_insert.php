a9979b6867bcc9d26fa4039f0501a68ea53a3afe
衣斐司郎 <>
2020-03-11 23:19:51 +0900

    PHPフォルダにて管理

---
+++ basic_insert.php
@@ -0,0 +1,15 @@
+<?php
+//新しいデータベースの場合は、先にphpMyAdminの権限設定を実施すること
+
+$user       = 'shiro';
+$password   = 'sunyou00';
+$dbName     = 'sunyou_maintenance_system';
+$host       = 'localhost';
+$dsn        = "mysql:host={$host};dbname={$dbName};charset=utf8";
+$pdo        = new PDO($dsn, $user, $password);
+$sql        = "INSERT INTO TEST_TABLE(name) VALUES (10)";
+$stm        = $pdo->prepare($sql);
+$stm        ->execute();
+$dsn        = NULL;
+
+?>
